6476205e284d994a89898011c1b42cec1baf6d61
CyrilJude <cyriljuda2004@gmail.com>
2025-01-22 14:52:50 +0530

    initial backend version

---
+++ ExpenseManagementApp/src/test/java/com/example/ExpenseManagementApp/ExpenseManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExpenseManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
