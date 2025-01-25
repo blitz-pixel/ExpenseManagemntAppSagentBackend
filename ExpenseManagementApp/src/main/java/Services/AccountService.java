f66729646a2fb3a04afb29e9e407fafaba5d4f23
blitz-pixel <charanhasini16@gmail.com>
2025-01-25 14:39:51 +0530

    Lombok have to work

--- ExpenseManagementApp/src/main/java/Services/AccountService.java
+++ ExpenseManagementApp/src/main/java/Services/AccountService.java
@@ -1,4 +1,9 @@
 package Services;
 
+import Model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AccountService {
 }
