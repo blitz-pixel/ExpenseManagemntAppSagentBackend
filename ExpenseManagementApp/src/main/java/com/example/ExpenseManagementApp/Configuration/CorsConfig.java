013edfac5f561d73c14de8956c0ff6f7f9db6a3c
CyrilJude <cyriljuda2004@gmail.com>
2025-01-29 11:52:43 +0530

    frontend commit

--- ExpenseManagementApp/src/main/java/com/example/ExpenseManagementApp/Configuration/CorsConfig.java
+++ ExpenseManagementApp/src/main/java/com/example/ExpenseManagementApp/Configuration/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000") // Allow requests from this origin
+                        .allowedOrigins("http://localhost:8080") // Allow requests from this origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow these HTTP methods
                         .allowedHeaders("*") // Allow all headers
                         .allowCredentials(true); // Allow credentials (e.g., cookies)
