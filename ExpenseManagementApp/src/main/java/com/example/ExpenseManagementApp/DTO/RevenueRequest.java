f1f1564586e1f47c1e2ec144adcdfdc8e0551e6f
CyrilJude <cyriljuda2004@gmail.com>
2025-02-02 22:50:53 +0530

    did something useful ig

---
+++ ExpenseManagementApp/src/main/java/com/example/ExpenseManagementApp/DTO/RevenueRequest.java
@@ -0,0 +1,41 @@
+package com.example.ExpenseManagementApp.DTO;
+
+public class RevenueRequest {
+    private Long accountId;
+    private String description;
+    private Double amount;
+    private Long categoryId;
+
+    // Getters and Setters
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+}
